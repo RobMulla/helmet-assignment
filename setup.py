151b66c41c1a4ab8d5395c3fa22d7c029778632c
Rob Mulla <rob.mulla@gmail.com>
2021-08-18 21:39:50 -0400

    setup code

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='helmet_assignment',
+    version='0.1.0',
+    description='Helper code for the 2021 NFL Kaggle Helmet Assignment Competition',
+    url='https://github.com/RobMulla/helmet-assignment',
+    author='Rob Mulla',
+    author_email='rob.mulla@gmail.com',
+    license='Apache License 2.0',
+    packages=['helmet_assignment'],
+    install_requires=['pandas', 'numpy'],
+)
